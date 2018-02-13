4924e9e97322ebf40d238dc428b25556dfcc9f41
rao4 <rohara239@gmail.com>
2018-02-12 20:58:59 -0500

    Added validation attributes to model, Added create and details  Views

--- AutomatedTellerMachine/Models/CheckingAccount.cs
+++ AutomatedTellerMachine/Models/CheckingAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,27 @@ namespace AutomatedTellerMachine.Models
     public class CheckingAccount
     {
         public int ID { get; set; }
+
+        [Required]
+        //[StringLength]
+        [RegularExpression(@"\d{6,10}", ErrorMessage = "Account Number must between 6-10 digits.")]
+        [Display(Name = "Account #")]
         public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        public string Name
+        {
+            get { return $"{FirstName} {LastName}"; }
+        }
+
+        [DataType(DataType.Currency)]
         public decimal Balance { get; set; }
     }
 }
